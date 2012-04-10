600e93ff844e80b3f11dc3021c7ca7194868ac1e
Cupcake <ivan@gamol.com.mx>
2012-04-10 14:46:41 -0500

    Zona Yoo Script
    Sctipt de display de cada cablera funcionando corréctamente

--- js/actions-zona.js
+++ js/actions-zona.js
@@ -1,13 +1,14 @@
 $(document).ready(function() {
-	
-	$('.cablera').each(function(){
-			$('.step1').click(function(){
-				$('.step1').fadeOut(500);
-				$('.step2').slideDown(750, 'easeInOutExpo').fadeIn(250);
-			});
-			$('.return').click(function(){
-				$('.step2').slideUp(1000, 'easeOutExpo').fadeOut(250);
-				$('.step1').fadeIn(500);
-			});
+	$('.step1').click(function(e){
+		var i = $(this).index('.step1');
+		// console.log(i);
+		$('.step1').eq(i).fadeOut(500);
+		$('.step2').eq(i).slideDown(750, 'easeInOutExpo').fadeIn(250);
+	});
+	$('.return').click(function(){
+		var i = $(this).index('.return');
+		// console.log(i);
+		$('.step2').eq(i).slideUp(1000, 'easeOutExpo').fadeOut(250);
+		$('.step1').eq(i).fadeIn(500);
 	});
 });
